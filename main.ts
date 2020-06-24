4cd90bb47316357fcdd18fb6a1a93161f20c22e8
xmaraider <67031779+xmaraider@users.noreply.github.com>
2020-06-24 09:40:39 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.SmallSquare)
     a = 1
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Yes)
+    a = 0
+    b = 0
+    c = 0
+    list = []
+})
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Scissors)
     a = 3
@@ -16,24 +23,90 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Chessboard)
     a = 2
 })
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Yes)
-    a = 0
-    b = 0
-    c = 0
-    list = []
-})
 let a = 0
 let c = 0
 let b = 0
 let list: number[] = []
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-    if (0 == 0) {
-    	
-    } else if (0 == 0) {
-    	
+    radio.setGroup(9)
+    if (c == 1) {
+        if (b == 1) {
+            if (a == 1) {
+                basic.showIcon(IconNames.Asleep)
+            } else if (a == 2) {
+                basic.showIcon(IconNames.Happy)
+            } else {
+                basic.showIcon(IconNames.Sad)
+            }
+        } else if (b == 2) {
+            if (a == 1) {
+                basic.showIcon(IconNames.Sad)
+            } else if (a == 2) {
+                basic.showIcon(IconNames.Happy)
+            } else {
+                basic.showIcon(IconNames.Asleep)
+            }
+        } else {
+            if (a == 1) {
+                basic.showIcon(IconNames.Happy)
+            } else if (a == 2) {
+                basic.showIcon(IconNames.Asleep)
+            } else {
+                basic.showIcon(IconNames.Happy)
+            }
+        }
+    } else if (c == 2) {
+        if (b == 1) {
+            if (a == 1) {
+                basic.showIcon(IconNames.Happy)
+            } else if (a == 2) {
+                basic.showIcon(IconNames.Happy)
+            } else {
+                basic.showIcon(IconNames.Asleep)
+            }
+        } else if (b == 2) {
+            if (a == 1) {
+                basic.showIcon(IconNames.Sad)
+            } else if (a == 2) {
+                basic.showIcon(IconNames.Asleep)
+            } else {
+                basic.showIcon(IconNames.Sad)
+            }
+        } else {
+            if (a == 1) {
+                basic.showIcon(IconNames.Asleep)
+            } else if (a == 2) {
+                basic.showIcon(IconNames.Sad)
+            } else {
+                basic.showIcon(IconNames.Happy)
+            }
+        }
     } else {
-    	
+        if (b == 1) {
+            if (a == 1) {
+                basic.showIcon(IconNames.Happy)
+            } else if (a == 2) {
+                basic.showIcon(IconNames.Asleep)
+            } else {
+                basic.showIcon(IconNames.Confused)
+            }
+        } else if (b == 2) {
+            if (a == 1) {
+                basic.showIcon(IconNames.Asleep)
+            } else if (a == 2) {
+                basic.showIcon(IconNames.Sad)
+            } else {
+                basic.showIcon(IconNames.Happy)
+            }
+        } else {
+            if (a == 1) {
+                basic.showIcon(IconNames.Happy)
+            } else if (a == 2) {
+                basic.showIcon(IconNames.Sad)
+            } else {
+                basic.showIcon(IconNames.Asleep)
+            }
+        }
     }
 })
