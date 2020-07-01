0c0b84e2aaf5a77b9c487721ade7d07d8e808f9b
xmaraider <67031779+xmaraider@users.noreply.github.com>
2020-07-01 09:56:26 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,38 @@
 radio.onReceivedNumber(function (receivedNumber) {
     list.push(receivedNumber)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.SmallSquare)
+    a = 1
+    radio.sendNumber(a)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Yes)
+    a = 0
+    b = 0
+    c = 0
+    list = []
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Scissors)
+    a = 3
+    radio.sendNumber(a)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Chessboard)
+    a = 2
+    radio.sendNumber(a)
+})
+let a = 0
+let c = 0
+let b = 0
+let list: number[] = []
+basic.showIcon(IconNames.Yes)
+list = []
+radio.setGroup(9)
+b = list[1]
+c = list[2]
+basic.forever(function () {
     if (list.length == 1) {
         if (a == 1) {
             if (b == 1) {
@@ -107,38 +140,4 @@ radio.onReceivedNumber(function (receivedNumber) {
             }
         }
     }
-})
-input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.SmallSquare)
-    a = 1
-    radio.sendNumber(a)
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Yes)
-    a = 0
-    b = 0
-    c = 0
-    list = []
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.showIcon(IconNames.Scissors)
-    a = 3
-    radio.sendNumber(a)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Chessboard)
-    a = 2
-    radio.sendNumber(a)
-})
-let a = 0
-let c = 0
-let b = 0
-let list: number[] = []
-basic.showIcon(IconNames.Yes)
-list = []
-radio.setGroup(9)
-b = list[1]
-c = list[2]
-basic.forever(function () {
-	
 })
